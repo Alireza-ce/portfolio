7562071b763d262a98915aa1aa45019641bcd00f
Alireza-ce <rezaei.alireza.ce@gmail.com>
2022-05-09 19:41:59 +0430

    add work part

--- src/components/work-experience/work-experience.tsx
+++ src/components/work-experience/work-experience.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
-import classes from './work-experience.module.scss'
+import { Player } from '@lottiefiles/react-lottie-player';
+import React, { useEffect, useRef } from 'react'
+import classes from './work-experience.module.scss';
+import animationData from './working-man.json';
+
 export default function WorkExperience() {
+
+  useEffect(() => {
+
+  }, [])
+
   return (
-    <div className={classes.work_page}>work</div>
+    <div className={classes.work_page}>
+      <div className={`${classes.container} ${classes.work_row}`}>
+        <div className={classes.work_experience}>
+          <p className={classes.title}>Work Experience</p>
+          <div className={classes.work_time_line}>
+            <div className={classes.company_info}>
+              <div className={classes.company_links}>
+                <p>Front end developer at</p>
+                <a href='#' className={classes.name}> Yara Information technology</a>
+              </div>
+              <p className={classes.date}>Oct 2020 - Sep 2022</p>
+              <p className={classes.description}>a lot of inforamtion come here . to haminja bia angular o react ro benevis b bold ya rangeshon ro avaz kn</p>
+            
+            </div>
+
+            <div className={classes.company_info}>
+              <div className={classes.company_links}>
+                <p>Front end developer at</p>
+                <a href='#' className={classes.name}> Yara Information technology</a>
+              </div>
+              <p className={classes.date}>Oct 2020 - Sep 2022</p>
+              <p className={classes.description}>a lot of inforamtion come here . to haminja bia angular o react ro benevis b bold ya rangeshon ro avaz kn</p>
+            
+            </div>
+            
+
+
+          </div>
+        </div>
+        <div className={classes.animation}>
+          <Player
+            autoplay
+            loop
+            src={animationData}
+            style={{ height: '300px', width: '100%' }}
+          >
+          </Player>
+        </div>
+      </div>
+    </div>
   )
 }
