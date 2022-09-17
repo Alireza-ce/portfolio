7a9304b417a9d6d7c3e52428b9e499c636a1a21b
Alireza-ce <rezaei.alireza.ce@gmail.com>
2022-09-17 23:29:46 +0430

    fix projects

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import classes from './about.module.scss';
 import github from './../../assest/img/github.png'
-import projectPic from './../../assest/img/p.png';
+import projectPic from './../../assest/img/pulsesensor.png';
+import pacmanPic from './../../assest/img/pacman.jpg';
+import expenses from './../../assest/img/expenses.jpg';
 import website from './../../assest/img/web.png';
+import heart from './../../assest/img/heart.png';
+import pacman from './../../assest/img/pacman.png';
+import budget from './../../assest/img/budget.png';
 
 export default function About() {
   return (
@@ -14,15 +19,13 @@ export default function About() {
           </p>
           <div className={classes.project_info}>
             <div className={classes.project_name}>
-              <p>Project name -- make this clicable to project website</p>
+              <p>Heart Rate Analysis System</p>
+              <img  src={heart} />
             </div>
-            <p className={classes.project_tech}>React / JS / SCSS / FireBase -- rang ina bar asas logo khode company bashe </p>
+            <p className={classes.project_tech}> Angular / Node js</p>
             <div className={classes.project_links}>
-              <a href="#">
+              <a href="https://github.com/Alireza-ce/pulse-sensor-with-arduino" target="_blank">
                 <img  src={github} alt="github" />
-              </a>
-              <a href="#">
-                <img className={classes.web} src={website} alt="github" />
               </a>
             </div>
 
@@ -32,38 +35,35 @@ export default function About() {
         <div className={`${classes.project_card} ${classes.health_project}`}>
           <div className={classes.project_info}>
             <div className={classes.project_name}>
-              <p>Project name</p>
+              <p>Pacman</p>
+              <img  src={pacman} />
             </div>
-            <p className={classes.project_tech}>React / JS / SCSS / FireBase -- rang ina bar asas logo khode company bashe </p>
+            <p className={classes.project_tech}> JS / CSS</p>
             <div className={classes.project_links}>
-              <a href="#">
+              <a href="https://github.com/Alireza-ce/pacman-javascript" target="_blank">
                 <img src={github} alt="github" />
               </a>
-              <a href="#">
-              <img className={classes.web} src={website} alt="github" />
+              <a href="https://pacman-clone-js.netlify.app/" target="_blank">
+              <img className={classes.web} src={website} alt="pacman" />
               </a>
             </div>
 
-            <img className={classes.project_pic} src={projectPic} />
+            <img className={classes.project_pic} src={pacmanPic} />
           </div>
         </div>
         <div className={`${classes.project_card}`}>
           <div className={classes.project_info}>
             <div className={classes.project_name}>
-              <p>Project name</p>
+              <p>Expenses Management</p>
+              <img  src={budget} />
             </div>
-            <p className={classes.project_tech}>React / JS / SCSS / FireBase -- rang ina bar asas logo khode company bashe </p>
+            <p className={classes.project_tech}>React / React Query /  SCSS / FireBase </p>
             <div className={classes.project_links}>
-              <a href="#">
+              <a href="https://github.com/Alireza-ce/expenses-management" target="_blank">
                 <img src={github} alt="github" />
               </a>
-              <a href="#">
-              <img className={classes.web} src={website} alt="github" />
-              </a>
             </div>
-
-            <img className={classes.project_pic} src={projectPic} />
-            <p>all project links come here</p>
+            <img className={classes.project_pic} src={expenses} />
           </div>
         </div>
 
